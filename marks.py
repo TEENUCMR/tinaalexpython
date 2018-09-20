6d3ad9d5c4dbf7617dd08b86e46ada9d0914f8ea
TEENUCMR <43022997+TEENUCMR@users.noreply.github.com>
2018-09-20 08:53:00 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,15 @@
+x=int(input("enter the marks "))
+#marks with grade
+if(x>=90):
+	print("a+")
+elif(x>=80) :
+	print("a")
+elif(x>=70):
+	print("b+")
+elif(x>=60):
+	print("b")
+elif(x>=40):
+	print("c")
+else:
+	print("fail")
+
