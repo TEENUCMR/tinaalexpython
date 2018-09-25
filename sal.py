304a296f233e9f381ca5c3dbf51200e97abafae4
TEENUCMR <43022997+TEENUCMR@users.noreply.github.com>
2018-09-25 12:26:25 +0530

    Add files via upload

---
+++ sal.py
@@ -0,0 +1,14 @@
+#add and sub
+a=10
+b=20
+def add():
+	print(a+b)
+
+
+add()
+
+def sub():
+	return a-b
+
+r=sub()
+print(r)
---
+++ world.py
@@ -0,0 +1,10 @@
+def greet(name):
+	if(name):
+		print("HEY ",name,"!")
+	else:
+		print("HELLO WORLD!")
+
+
+a=input("What is your name?")
+
+greet(a)
